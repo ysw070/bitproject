2f109362db70573314596a69a50b5cfe10ceeca5
soonjongnim <soonjongnim@naver.com>
2016-01-06 18:01:35 +0900

    .

--- gameX1/src/main/java/java76/pms/Huu.java
+++ gameX1/src/main/java/java76/pms/Huu.java
@@ -5,6 +5,7 @@ public class Huu {
   public static void main(String[] args) {
     System.out.println("hello?");
     SR();
+    SR2();
   }
   
   private static void SR() {
@@ -13,6 +14,14 @@ public class Huu {
     System.out.println("히힛");
     System.out.println("승렬이");
     System.out.println("히힛");
+  }
+  
+  private static void SR2() {
+    System.out.println("asas");
+    System.out.println("as");
+    System.out.println("as");
+    System.out.println("as");
+    System.out.println("asaa");
   }
 
 }
